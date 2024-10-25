51b22192f93918480efc500bbd7151c37e8b2076
GabrielCardosoLIma <limag656@gmail.com>
2024-10-24 22:54:44 -0300

    chore: initial project configuration

---
+++ src/main/java/com/connectcampus/api/ConnectcampusApiApplication.java
@@ -0,0 +1,13 @@
+package com.connectcampus.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectcampusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectcampusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connectcampus/api/ConnectcampusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectcampus.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectcampusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
